1145004711f7a5e8faa1979b4656bdde22a07a47
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-07-25 23:41:12 +0300

    pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,16 @@
 def pascal_triangle(n):
     """
     n is the number of rows in Pascal's Triangle generated
-    returns aA list of lists where each inner list represents a row in Pascal's Triangle.
+    returns a list of list in pascal's triangle
     """
     if n <= 0:
         return []
 
     tri = []
-    
+
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = tri[i - 1][j - 1] + tri[i - 1][j]
         tri.append(row)
     return tri
-
