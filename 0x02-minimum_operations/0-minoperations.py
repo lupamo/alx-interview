609af154e5613224a176f4961ea46e0d2af4352f
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-08-06 18:51:21 +0300

    minimum ops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    """
+    Minimum operations to reach n from 1
+    """
+
+    ops = 0
+    div = 2
+
+    while n > 1:
+        while n % div == 0:
+            ops += div
+            n //= div
+        div += 1
+
+    return ops
