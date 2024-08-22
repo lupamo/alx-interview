4e862124a32004969cf7ffbd5c8d6413593c1df1
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-08-22 07:58:12 +0300

    validated

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,21 +5,32 @@ UTF-8 validation module
 """
 
 
+def get_bits(num):
+    """returns leading sets of bits"""
+    set_bits = 0
+    lender = 1 << 7
+    while lender & num:
+        set_bits += 1
+        lender = lender >> 1
+    return set_bits
+
+
 def validUTF8(data):
     """
     a method that validates a given dataset
     representing UTF-8
     """
 
-    for byte in data:
-        lsb = byte & 0xFF
-
-        if lsb >= 128:
-            return False
-        elif lsb > 192:
-            return False
-        elif lsb > 224:
-            return False
-        elif lsb > 240:
-            return False
-    return True
+    cnt = 0
+    for i in range(len(data)):
+        if cnt == 0:
+            cnt = get_bits(data[i])
+            if cnt == 0:
+                continue
+            if cnt == 1 or cnt > 4:
+                return False
+            else:
+                if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+                    return False
+            cnt -= 1
+    return cnt == 0
